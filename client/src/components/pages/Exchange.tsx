1040fffa9347015997b8af34af58b07e0ff9e1be
joselrodrigues <ing.joserodrigues@gmail.com>
2023-01-07 01:30:09 -0300

    add socket io to the frontend

--- client/src/components/pages/Exchange.tsx
+++ client/src/components/pages/Exchange.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 import { Dropdown } from '../atoms/Dropdown';
 
+// I could't make socket work with internal docker port is like somenthing is happening with al the pulling
+const socket = io('http://localhost:4000');
 const Exchange = () => {
+  useEffect(() => {
+    console.log('useEffect se ejecutó', socket.connect);
+    socket.on('update', (data) => {
+      console.log('UPDATE se ejecutó', data);
+    });
+  }, []);
+  console.log('que paso');
   return (
     <div>
       <div>
